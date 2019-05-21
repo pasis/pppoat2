4041edff098ef3f11de0a457a5f60654144cd71f
Dmitry Podgorny <pasis.ua@gmail.com>
2019-05-21 11:49:54 +0300

    Update year in copyrights

--- src/pppoat.c
+++ src/pppoat.c
@@ -1,7 +1,7 @@
 /* pppoat.c
  * PPP over Any Transport -- PPPoAT context
  *
- * Copyright (C) 2012-2018 Dmitry Podgorny <pasis.ua@gmail.com>
+ * Copyright (C) 2012-2019 Dmitry Podgorny <pasis.ua@gmail.com>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
