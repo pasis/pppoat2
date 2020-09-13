fa2726777633b10b1e4eef3357e64686a9210c50
Dmitry Podgorny <pasis.ua@gmail.com>
2020-09-14 01:57:04 +0300

    modules/xmpp: XEP-0092 (software version)

--- src/modules/tp_xmpp.c
+++ src/modules/tp_xmpp.c
@@ -101,6 +101,7 @@ enum {
 
 #define XMPP_NS_XEP_0091 "jabber:x:delay"
 #define XMPP_NS_XEP_0203 "urn:xmpp:delay"
+#define XMPP_NS_XEP_0092 "jabber:iq:version"
 
 static const xmpp_mem_t tp_xmpp_mem;
 static const xmpp_log_t tp_xmpp_log;
@@ -113,6 +114,9 @@ static void tp_xmpp_conn_handler(xmpp_conn_t * const         conn,
 				 xmpp_stream_error_t * const stream_error,
 				 void * const                userdata);
 static int tp_xmpp_message_handler(xmpp_conn_t * const   conn,
+				   xmpp_stanza_t * const stanza,
+				   void * const          userdata);
+static int tp_xmpp_version_handler(xmpp_conn_t * const   conn,
 				   xmpp_stanza_t * const stanza,
 				   void * const          userdata);
 static int tp_xmpp_send(struct tp_xmpp_ctx *ctx, struct pppoat_packet *pkt);
@@ -166,6 +170,7 @@ static int tp_xmpp_init(struct pppoat_module *mod, struct pppoat_conf *conf)
 	ctx = pppoat_alloc(sizeof *ctx);
 	if (ctx == NULL)
 		return P_ERR(-ENOMEM);
+	memset(ctx, 0, sizeof *ctx);
 
 	rc = tp_xmpp_conf_parse(ctx, conf);
 	PPPOAT_ASSERT(rc == 0); /* XXX */
@@ -258,6 +263,16 @@ static void tp_xmpp_worker(struct pppoat_thread *thread)
 	pppoat_debug("xmpp", "Event loop finished.");
 }
 
+static void tp_xmpp_handlers_register(struct tp_xmpp_ctx *ctx)
+{
+	/* Message handler */
+	xmpp_handler_add(ctx->txc_xmpp_conn, tp_xmpp_message_handler,
+			 NULL, "message", NULL, ctx);
+	/* Software Version handler */
+	xmpp_handler_add(ctx->txc_xmpp_conn, tp_xmpp_version_handler,
+			 XMPP_NS_XEP_0092, "iq", "get", ctx);
+}
+
 static int tp_xmpp_run(struct pppoat_module *mod)
 {
 	struct tp_xmpp_ctx *ctx = mod->m_userdata;
@@ -271,8 +286,7 @@ static int tp_xmpp_run(struct pppoat_module *mod)
 
 	xmpp_conn_set_jid(ctx->txc_xmpp_conn, ctx->txc_jid);
 	xmpp_conn_set_pass(ctx->txc_xmpp_conn, ctx->txc_passwd);
-	xmpp_handler_add(ctx->txc_xmpp_conn, tp_xmpp_message_handler,
-			 NULL, "message", NULL, ctx);
+	tp_xmpp_handlers_register(ctx);
 
 	ctx->txc_connected = false;
 	ctx->txc_stopping = false;
@@ -472,10 +486,7 @@ static void tp_xmpp_conn_handler(xmpp_conn_t * const         conn,
 	pppoat_debug("xmpp", "Connection handler is called for event: %s",
 		     tp_xmpp_conn_event_to_str(status));
 
-	if (status == XMPP_CONN_CONNECT)
-		ctx->txc_connected = true;
-	else
-		ctx->txc_connected = false;
+	ctx->txc_connected = status == XMPP_CONN_CONNECT;
 
 	if (ctx->txc_stopping) {
 		xmpp_stop(ctx->txc_xmpp_ctx);
@@ -543,6 +554,82 @@ static int tp_xmpp_message_handler(xmpp_conn_t * const   conn,
 	pppoat_queue_enqueue(&ctx->txc_recv_q, pkt);
 	pppoat_semaphore_post(&ctx->txc_recv_sem);
 
+	return 1;
+}
+
+static int tp_xmpp_version_handler(xmpp_conn_t * const   conn,
+				   xmpp_stanza_t * const stanza,
+				   void * const          userdata)
+{
+	struct tp_xmpp_ctx *ctx = userdata;
+	xmpp_ctx_t         *xmpp_ctx = ctx->txc_xmpp_ctx;
+	xmpp_stanza_t      *iq;
+	xmpp_stanza_t      *query;
+	xmpp_stanza_t      *item;
+	xmpp_stanza_t      *text;
+	const char         *sw_name;
+	const char         *sw_version;
+
+	PPPOAT_ASSERT(tp_xmpp_ctx_invariant(ctx));
+
+#ifdef PACKAGE_NAME
+	sw_name = PACKAGE_NAME;
+#else
+	sw_name = "pppoat";
+#endif /* PACKAGE_NAME */
+
+#ifdef PACKAGE_VERSION
+	sw_version = PACKAGE_VERSION;
+#else
+	sw_version = "0";
+#endif /* PACKAGE_VERSION */
+
+	iq = xmpp_stanza_reply(stanza);
+	if (iq == NULL)
+		goto quit;
+	xmpp_stanza_set_type(iq, "result");
+
+	query = xmpp_stanza_new(xmpp_ctx);
+	if (query == NULL)
+		goto quit;
+	xmpp_stanza_set_ns(query, XMPP_NS_XEP_0092);
+	xmpp_stanza_set_name(query, "query");
+	xmpp_stanza_add_child(iq, query);
+	xmpp_stanza_release(query);
+
+	item = xmpp_stanza_new(xmpp_ctx);
+	if (item == NULL)
+		goto quit;
+	xmpp_stanza_set_name(item, "name");
+	xmpp_stanza_add_child(query, item);
+	xmpp_stanza_release(item);
+
+	text = xmpp_stanza_new(xmpp_ctx);
+	if (text == NULL)
+		goto quit;
+	xmpp_stanza_set_text(text, sw_name);
+	xmpp_stanza_add_child(item, text);
+	xmpp_stanza_release(text);
+
+	item = xmpp_stanza_new(xmpp_ctx);
+	if (item == NULL)
+		goto quit;
+	xmpp_stanza_set_name(item, "version");
+	xmpp_stanza_add_child(query, item);
+	xmpp_stanza_release(item);
+
+	text = xmpp_stanza_new(xmpp_ctx);
+	if (text == NULL)
+		goto quit;
+	xmpp_stanza_set_text(text, sw_version);
+	xmpp_stanza_add_child(item, text);
+	xmpp_stanza_release(text);
+
+	xmpp_send(conn, iq);
+
+quit:
+	if (iq != NULL)
+		xmpp_stanza_release(iq);
 	return 1;
 }
 
